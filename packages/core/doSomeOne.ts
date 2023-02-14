21a9edc75b38c813a9b0371992ccff5e0d3bd58d
chenglu <chenglu4343@qq.com>
2023-02-15 01:18:58 +0800

    ✨ feat: add doSomeOne

---
+++ packages/core/doSomeOne.ts
@@ -0,0 +1,6 @@
+export function doSomeOne(strategy: [condition: boolean, exec: () => void][]) {
+  strategy.some(([condition, exec]) => {
+    condition && exec();
+    return condition;
+  });
+}
--- packages/core/index.ts
+++ packages/core/index.ts
@@ -1,1 +1,3 @@
 export const name='utils'
+
+export * from './doSomeOne'
