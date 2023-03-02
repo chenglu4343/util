3c94ff176f39c77850cec302762d150a2807e15d
jiale.qiu <jiale.qiu@unionman.com.cn>
2023-03-02 14:43:27 +0800

    🐳 chore: update config

--- packages/core/build.config.ts
+++ packages/core/build.config.ts
@@ -1,13 +1,13 @@
 import { defineBuildConfig } from 'unbuild'
 
 export default defineBuildConfig({
-  outDir: './dist',
-  entries: [
-    './src/index',
-  ],
-  declaration: true,
-  clean: true,
-  rollup: {
-    emitCJS: true,
-  },
+	outDir: './dist',
+	entries: ['./src/index'],
+	rootDir: '.',
+	declaration: true,
+	dependencies: ['lodash-es'],
+	clean: true,
+	rollup: {
+		emitCJS: true,
+	},
 })
